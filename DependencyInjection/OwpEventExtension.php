a882a188f9aedbd4f3c2fc11e3d17a6ce9250e48
qguillou <guillou.quentin.29@gmail.com>
2019-11-24 19:50:58 +0100

    Adding dependencies

---
+++ DependencyInjection/OwpEventExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Owp\OwpEvent\DependencyInjection;
+
+use Knp\Menu\ItemInterface;
+use Knp\Menu\Matcher\Voter\VoterInterface;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+
+class OwpEventExtension extends Extension
+{
+    /**
+     * Handles the knp_menu configuration.
+     *
+     * @param array            $configs   The configurations being loaded
+     * @param ContainerBuilder $container
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
+    }
+}
